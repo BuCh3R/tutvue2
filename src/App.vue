72059bbecc6387e598ed9cdc606aa7acf65162a5
Morten Buch <mobu26304@edu.ucl.dk>
2024-04-03 11:30:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import books from '@/components/books.vue';
+</script>
+
+<template>
+  <books />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/books.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+const bøger = ref([
+    {
+        titel: "To kill a mockingbird",
+        forfatter: "Harper Lee"
+    },
+    {
+        titel: "The seven husbands of Evelyn Hugo",
+        forfatter: "Taylor Jenkins Reid"
+    },
+    {
+        titel: "The fault in our stars",
+        forfatter: "John Green"
+    },
+    {
+        titel: "The book thief",
+        forfatter: "Markus zusak"
+    },
+    {
+        titel: "The hunger games",
+        forfatter: "Suzanne Collins"
+    }
+]);
+</script>
+
+
+<template>
+    <ul>
+        <li v-for="i in bøger" :key="i.titel">
+            Titel: {{ i.titel }}
+            Forfatter: {{ i.forfatter }}
+        </li>
+    </ul>
+</template>
